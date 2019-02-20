119b7b7b80836e160676d7c2daefddaf4896d166
SergeyMuratkin <SergeyMuratkin@yandex.ru>
2019-02-20 11:12:16 +0300

    MySql

--- src/main/java/ru/restlogdate/controller/MainController.java
+++ src/main/java/ru/restlogdate/controller/MainController.java
@@ -3,8 +3,6 @@ package ru.restlogdate.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.restlogdate.Utilities;
 import ru.restlogdate.model.LogDate;
@@ -12,11 +10,14 @@ import ru.restlogdate.model.LogStatus;
 import ru.restlogdate.service.LogDateService;
 import ru.restlogdate.service.LogDateServiceStencil;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 @RestController
 public class MainController {
+
     private LogDateServiceStencil logDateService;
-    public MainController() {
-    }
 
     @Autowired
     public void setLogDateService(LogDateServiceStencil logDateService) {
@@ -24,30 +25,27 @@ public class MainController {
     }
 
     //    @POST
-//    @GET
-//    @Path("/")
-//    @Produces("application/xml")
+    @GET
+    @Path("/")
+    @Produces("application/xml")
     // @ResponseStatus(value = HttpStatus.OK)
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/xml")
+//    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/xml")
 
     //   public int getLogDate () throws InterruptedException {
     public LogDate getLogDate() {
-
-        LogDateService logDateService = new LogDateService();
         LogDate logDate = logDateService.create();
         int id = logDate.getGUID();
         HttpStatus.valueOf(202);                        // не возвращает статус
         logDateService.update(id, LogStatus.running);
         Utilities.DeferMetod(() -> logDateService.update(id, LogStatus.finished), 120);
-
         //     return id;
         return logDate;//не получается выводить только одно id
     }
 
-    //    @GET
-//    @Path(value = "/{id}")
-//    @Produces("application/xml")
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/xml")
+    @GET
+    @Path(value = "/{id}")
+    @Produces("application/xml")
+    //  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/xml")
     // Ну почему, почему ты не работаешь ???!!
     public LogDate getLogDateID(@PathVariable(value = "id") int id) {
         // public LogStatus getLogDateID() {
